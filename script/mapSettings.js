dcf65c1bc1346dc663fcd9347a1a00502c3231c3
can2399 <32220576+can2399@users.noreply.github.com>
2021-10-11 09:02:37 +0300

    Update mapSettings.js

--- script/mapSettings.js
+++ script/mapSettings.js
@@ -4,6 +4,6 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
+L.marker([39.941974, 32.854371]).addTo(map)
     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
     .openPopup();
