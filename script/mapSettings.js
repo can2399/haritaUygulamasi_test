56d5ae47bc13fc16c8a43306efb870d7f4c45d88
can2399 <32220576+can2399@users.noreply.github.com>
2021-10-11 22:22:58 +0300

    Update mapSettings.js

--- script/mapSettings.js
+++ script/mapSettings.js
@@ -11,6 +11,7 @@ const zoom = 18;
 const lat = 39.941974;
 const lng = 32.854371;
 
+
 // calling map
 const map = L.map('map', config).setView([lat, lng], zoom);
 
@@ -29,3 +30,17 @@ L.marker([39.941974, 32.854371]).addTo(map)
     .bindPopup('<b>TESPİT DURUMU</b><br><br>Tespiti Yapılan : 230<br>Tespit Yapılacak : 200<br>İdari Yoldan Tescil : 29')
     .openPopup();
 
+// coordinate array with popup text
+let points = [
+  [39.96974, 32.854371, "point 1"],
+  [39.931974, 32.454371, "point 2"],
+  [39.961974, 32.854371, "point 3"],
+  [39.911974, 32.254371, "point 4"],
+];
+
+// loop that adds many markers to the map
+for (let i = 0; i < points.length; i++) {
+  const [lat, lng, popupText] = points[i];
+
+  marker1 = new L.marker([lat, lng]).bindPopup(popupText).addTo(map);
+}
