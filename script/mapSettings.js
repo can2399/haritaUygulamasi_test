5bea5efd0b93dab3f49b4b56a0d94e3a3994123b
can2399 <32220576+can2399@users.noreply.github.com>
2021-10-11 09:08:54 +0300

    Update mapSettings.js

--- script/mapSettings.js
+++ script/mapSettings.js
@@ -5,5 +5,5 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 L.marker([39.941974, 32.854371]).addTo(map)
-    .bindPopup('<b>TESPİT DURUMU</b><br>Tespiti Yapılan : 230<br>Tespit Yapılacak : 200<br>İdari Yoldan Tescil : 29')
+    .bindPopup('<b>TESPİT DURUMU</b><br><br>Tespiti Yapılan : 230<br>Tespit Yapılacak : 200<br>İdari Yoldan Tescil : 29')
     .openPopup();
