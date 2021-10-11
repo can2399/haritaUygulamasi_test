ce008024c770f2db6e7827df7cc136d77c3c552f
can2399 <32220576+can2399@users.noreply.github.com>
2021-10-11 09:31:12 +0300

    Update mapSettings.js

--- script/mapSettings.js
+++ script/mapSettings.js
@@ -18,6 +18,10 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+// reactivate zoom at the desired location
+// [topleft, topright, bottomleft, bottomright]
+L.control.zoom({ position: "topright" }).addTo(map);
+
 L.marker([39.941974, 32.854371]).addTo(map)
     .bindPopup('<b>TESPİT DURUMU</b><br><br>Tespiti Yapılan : 230<br>Tespit Yapılacak : 200<br>İdari Yoldan Tescil : 29')
     .openPopup();
