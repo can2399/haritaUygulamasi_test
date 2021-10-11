db5182a90cdd5471bc1db3cf05cc507cd4866d44
can2399 <32220576+can2399@users.noreply.github.com>
2021-10-11 20:52:49 +0300

    Update mapSettings.js

--- script/mapSettings.js
+++ script/mapSettings.js
@@ -22,6 +22,10 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // [topleft, topright, bottomleft, bottomright]
 L.control.zoom({ position: "topright" }).addTo(map);
 
+// Sclae control
+L.control.scale().addTo(map);
+
+
 L.marker([39.941974, 32.854371]).addTo(map)
     .bindPopup('<b>TESPİT DURUMU</b><br><br>Tespiti Yapılan : 230<br>Tespit Yapılacak : 200<br>İdari Yoldan Tescil : 29')
     .openPopup();
