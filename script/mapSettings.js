a46b6efba49b35f3839b56875a3d3fe083fa7d28
can2399 <32220576+can2399@users.noreply.github.com>
2021-10-12 05:42:27 +0300

    Update mapSettings.js

--- script/mapSettings.js
+++ script/mapSettings.js
@@ -49,6 +49,7 @@ L.control.scale('metric').addTo(map);
 }
 */
 
+/*
 let points = [
   ["Program","Akyurt","Ahmet Adil","20 Eylül - 20 Ekim",30,0,40.11467846572961,33.23448936169003],
   ["Program","Akyurt","Çam","20 Eylül - 20 Ekim",29,29,40.138222906247115,33.17026329614371],
@@ -56,6 +57,59 @@ let points = [
   ["Program Dışı","Altındağ","Aydıncık","20 Eylül - 20 Ekim",12,10,40.038399244170044,33.06638317260862],
   ["İdari Yoldan Tescil","Kalecik","Çandır","20 Eylül - 20 Ekim",40,40,40.260824057803084,33.467988059119065]
 ];
+*/
+let points = [
+        {
+            tespitDurum: "Program",
+            ilce: "Akyurt",
+            mahalle: "Ahmet Adil",
+            donem: "20 Eylül - 20 Ekim",
+            tasinmazSayisi: 30,
+            tespitiYapilan: 0,
+            enlem: 40.11467846572961,
+            boylam: 33.23448936169003
+        },
+        {
+            tespitDurum: "Program",
+            ilce: "Akyurt",
+            mahalle: "Çam",
+            donem: "20 Eylül - 20 Ekim",
+            tasinmazSayisi: 29,
+            tespitiYapilan: 29,
+            enlem: 40.138222906247115,
+            boylam: 33.17026329614371
+        },
+        {
+            tespitDurum: "Program Dışı",
+            ilce: "Altındağ",
+            mahalle: "Kavaklı",
+            donem: "20 Eylül - 20 Ekim",
+            tasinmazSayisi: 45,
+            tespitiYapilan: 40,
+            enlem: 40.0072035497873,
+            boylam: 33.04279069250751
+        },
+        {
+            tespitDurum: "Program Dışı",
+            ilce: "Altındağ",
+            mahalle: "Aydıncık",
+            donem: "20 Eylül - 20 Ekim",
+            tasinmazSayisi: 12,
+            tespitiYapilan: 10,
+            enlem: 40.038399244170044,
+            boylam: 33.06638317260862
+        },
+        {
+            tespitDurum: "İdari Yoldan Tescil",
+            ilce: "Kalecik",
+            mahalle: "Çandır",
+            donem: "20 Eylül - 20 Ekim",
+            tasinmazSayisi: 40,
+            tespitiYapilan: 40,
+            enlem: 40.260824057803084,
+            boylam: 33.467988059119065
+        }
+    ];
     
 // loop that adds many markers to the map
 for (let i = 0; i < points.length; i++) {
