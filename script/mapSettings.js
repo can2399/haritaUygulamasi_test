73a54a1675e6f456e3ef50beaf64bcf5a2bfdf9a
can2399 <32220576+can2399@users.noreply.github.com>
2021-10-13 05:59:19 +0300

    Update mapSettings.js

--- script/mapSettings.js
+++ script/mapSettings.js
@@ -35,7 +35,7 @@ var googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
     subdomains:['mt0','mt1','mt2','mt3']
 });
 var openTopo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png"', {
-    attribution: '&copy; '
+    attribution: '&copy; ',
     maxZoom: 16,
     subdomains:['mt0','mt1','mt2','mt3']
 }).addTo(map);
