3b895fa2f94fa10c388dad54869a6c87f1be049f
can2399 <32220576+can2399@users.noreply.github.com>
2021-10-13 04:58:14 +0300

    Update mapSettings.js

--- script/mapSettings.js
+++ script/mapSettings.js
@@ -20,17 +20,17 @@ var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 var googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{
-    attribution: '&copy; ,
+    attribution: '&copy; ',
     maxZoom: 20,
     subdomains:['mt0','mt1','mt2','mt3']
 });
 var googleHybrid = L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}',{
-    attribution: '&copy; ,
+    attribution: '&copy; ',
     maxZoom: 20,
     subdomains:['mt0','mt1','mt2','mt3']
 });
 var googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
-    attribution: '&copy; ,
+    attribution: '&copy; ',
     maxZoom: 20,
     subdomains:['mt0','mt1','mt2','mt3']
 });
