7bc1f0213256e3456d54a93615bd7e9c5c5e2e0f
can2399 <32220576+can2399@users.noreply.github.com>
2021-10-15 10:27:39 +0300

    Update mapSettings.js

--- script/mapSettings.js
+++ script/mapSettings.js
@@ -1,8 +1,10 @@
- var mrkCurrentLocation;
+const map;
+var mrkCurrentLocation;
 
 $(document).ready(function(){
  
-   // config map
+// config map
+ 
 let config = {
     minZoom: 7,
     maxZoom: 18,
@@ -19,54 +21,9 @@ let config = {
   
   // calling map
   
-  const map = L.map('map', config).setView([lat, lng], zoom);
+  map = L.map('map', config).setView([lat, lng], zoom);
   //  method fitBounds sets a map view                 
   
-   /*
-  // Event Örnekleri
-  // event handler on left click - Sol tuşa basınca koordinat ve zoom bilgisi gelsin
-       map.on('click' , function (e) {
-           alert(e.latlng.toString());
-           alert(myMap.getZoom());
-       });
-   
-  // right click - sağ tuşa basınca nokta eklesin - noktaya tıklayınca koordinat bilgisi gelsin
-        map.on('contextmenu', function (e) {
-            L.marker(e.latlng).addTo(map).bindPopup(e.latlng.toString());
-        })
-  
-  */
-   
-   // Locale - L tuşuna basınca konum a gider
-   
-    // call locate method
-   
-     
-
-        map.on('keypress', function (e) {
-            if (e.originalEvent.key = 'l'){
-                map.locate();
-            }
-        });
-
-        map.on('locationfound', function (e) {
-
-            if(mrkCurrentLocation){
-
-                mrkCurrentLocation.remove();
-            }
-            mrkCurrentLocation = L.circleMarker(e.latlng).addTo(myMap);
-            map.setView(e.latlng, 14);
-
-        });
-
-        map.on('locationerror', function (e) {
-
-            alert("location was not found");
-
-        });
-   
-   
   var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
@@ -113,12 +70,57 @@ let config = {
   // Sclae control
   L.control.scale('metric').addTo(map);
   
+   /*
+  // Event Örnekleri
+  // event handler on left click - Sol tuşa basınca koordinat ve zoom bilgisi gelsin
+       map.on('click' , function (e) {
+           alert(e.latlng.toString());
+           alert(myMap.getZoom());
+       });
+   
+  // right click - sağ tuşa basınca nokta eklesin - noktaya tıklayınca koordinat bilgisi gelsin
+        map.on('contextmenu', function (e) {
+            L.marker(e.latlng).addTo(map).bindPopup(e.latlng.toString());
+        })
   
+  */
+   
+   // Locale - L tuşuna basınca konum a gider
+   
+    // call locate method
+   
+     
+
+        map.on('keypress', function (e) {
+            if (e.originalEvent.key = 'l'){
+                map.locate();
+            }
+        });
+
+        map.on('locationfound', function (e) {
+
+            if(mrkCurrentLocation){
+
+                mrkCurrentLocation.remove();
+            }
+            mrkCurrentLocation = L.circleMarker(e.latlng).addTo(myMap);
+            map.setView(e.latlng, 14);
+
+        });
+
+        map.on('locationerror', function (e) {
+
+            alert("location was not found");
+
+        });
+ 
+ 
  // get user location on button click
     $('#get_user_location_id').click(function () {
         map.locate();
     })
   
+ 
 
       
 });
